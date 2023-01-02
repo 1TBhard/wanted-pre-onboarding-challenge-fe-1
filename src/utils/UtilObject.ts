f0c2e337d90d4d5a89ea97676182651a83a748bf
1TBhard <was1383@naver.com>
2023-01-02 20:51:47 +0900

    UtilObject 추가

---
+++ src/utils/UtilObject.ts
@@ -0,0 +1,28 @@
+interface ObjectType {
+	[key: string]: any;
+}
+
+class UtilObject {
+	static isEmpty(obj: ObjectType) {
+		return Object.keys(obj).length === 0;
+	}
+
+	static removeFalsy(params?: any) {
+		const copyParam = {
+			...params,
+		};
+
+		const propNameList = Object.getOwnPropertyNames(copyParam);
+
+		for (let i = 0; i < propNameList.length; i += 1) {
+			const propName = propNameList[i];
+			if (copyParam[propName] === null || copyParam[propName] === undefined) {
+				delete copyParam[propName];
+			}
+		}
+
+		return copyParam;
+	}
+}
+
+export default UtilObject;
