ac87b069f6cf38077ebaa3d04e856544009e6f44
1TBhard <was1383@naver.com>
2023-01-02 21:28:39 +0900

    Button disabled props 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,15 +3,22 @@ import { ButtonHTMLAttributes, CSSProperties } from "react";
 export const Button = ({
 	label,
 	style,
+	disabled = false,
 	buttonProp,
 }: {
 	label: string;
 	style?: CSSProperties;
+	disabled?: boolean;
 	buttonProp?: ButtonHTMLAttributes<HTMLButtonElement>;
 }) => {
 	return (
 		<div>
-			<button className='normal-btn' {...buttonProp} {...style}>
+			<button
+				disabled={disabled}
+				className={`normal-btn ${disabled ? "normal-btn__no-active" : ""}`}
+				{...buttonProp}
+				style={{ ...style }}
+			>
 				{label}
 			</button>
 		</div>
