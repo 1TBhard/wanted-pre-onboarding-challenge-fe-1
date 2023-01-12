2c4d58bab20686733b645280efb8595a8eebff5a
1TBhard <was1383@naver.com>
2023-01-12 22:51:03 +0900

    Modal 스타일 변경

--- src/components/common/Modal/Modal.style.ts
+++ src/components/common/Modal/Modal.style.ts
@@ -2,13 +2,15 @@ import { ModalProps } from "src/components/common/Modal/indext";
 import styled from "styled-components";
 
 export const Background = styled.div<Pick<ModalProps, "isShow">>`
-	display: ${(props) => (props.isShow ? "block" : "none")};
+	display: ${(props) => (props.isShow ? "flex" : "none")};
+	align-items: center;
+	justify-content: center;
 	position: absolute;
 	top: 0;
 	left: 0;
 	width: 100vw;
 	height: 100vh;
-	z-index: 2;
+	z-index: 9999;
 	background-color: #57575780;
 `;
 
@@ -20,7 +22,9 @@ export const Frame = styled.div`
 	z-index: 3;
 `;
 
-export const Header = styled.div``;
+export const Header = styled.div`
+	margin-bottom: 20px;
+`;
 
 export const Title = styled.h2`
 	text-align: center;
