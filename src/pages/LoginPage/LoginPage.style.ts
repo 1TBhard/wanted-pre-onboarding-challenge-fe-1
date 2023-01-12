359c8a7a70e56c13fe16490f31f822524d170c56
1TBhard <was1383@naver.com>
2023-01-12 15:33:18 +0900

    LoginPage style 분리

---
+++ src/pages/LoginPage/LoginPage.style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Form = styled.form``;
+
+export const Title = styled.h1`
+	text-align: center;
+	margin-bottom: 20px;
+	font-size: 32px;
+`;
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -7,6 +7,7 @@ import { FormEventHandler, useState } from "react";
 import { LabelInput } from "src/components/common/LabelInput";
 import { MainLayout } from "src/components/common/MainLayout";
 import { useNavigate } from "react-router-dom";
+import * as Styled from "./LoginPage.style";
 
 export const LoginPage = () => {
 	const navigate = useNavigate();
@@ -33,36 +34,45 @@ export const LoginPage = () => {
 	};
 
 	return (
-		<MainLayout title='로그인'>
-			<form id={FORM_ID.SIGN_IN} onSubmit={onSubmit}>
-				<LabelInput
-					label='이메일'
-					name='email'
-					type='text'
-					value={email}
-					onChange={(e) => {
-						setEmail(e.target.value);
-					}}
-				/>
+		<MainLayout>
+			<FlexBox height={"100%"} flexDirection='column' justifyContent={"center"}>
+				<Styled.Form id={FORM_ID.SIGN_IN} onSubmit={onSubmit}>
+					<FlexBox
+						flexDirection='column'
+						justifyContent={"center"}
+						alignItems='center'
+					>
+						<Styled.Title>로그인</Styled.Title>
 
-				<LabelInput
-					label='비밀번호'
-					name='password'
-					type='password'
-					value={password}
-					onChange={(e) => {
-						setPassword(e.target.value);
-					}}
-				/>
+						<LabelInput
+							label='이메일'
+							name='email'
+							type='text'
+							value={email}
+							onChange={(e) => {
+								setEmail(e.target.value);
+							}}
+						/>
 
-				<FlexBox justifyContent='center'>
-					<Button
-						disabled={!(email && password)}
-						label='로그인'
-						htmlType='submit'
-					/>
-				</FlexBox>
-			</form>
+						<LabelInput
+							label='비밀번호'
+							name='password'
+							type='password'
+							value={password}
+							onChange={(e) => {
+								setPassword(e.target.value);
+							}}
+						/>
+
+						<Button
+							disabled={!(email && password)}
+							label='로그인'
+							type='submit'
+							width={"100%"}
+						/>
+					</FlexBox>
+				</Styled.Form>
+			</FlexBox>
 		</MainLayout>
 	);
 };
