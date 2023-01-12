aa81cce640a5e5c546da9479785c2d6a00045836
1TBhard <was1383@naver.com>
2023-01-12 21:01:54 +0900

    TodoListMenu 추가

---
+++ src/components/main/TodoListMenu/TodoListMenu.style.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Frame = styled.div``;
+
+export const MenuHeader = styled.div`
+	text-align: center;
+	border-bottom: 1px solid black;
+`;
+
+export const MenuItem = styled.div`
+	width: 200px;
+	overflow: hidden;
+	text-overflow: ellipsis;
+
+	padding: 10px 0;
+
+	cursor: pointer;
+
+	&:hover {
+		color: #219ebc;
+	}
+`;
---
+++ src/components/main/TodoListMenu/index.tsx
@@ -0,0 +1,46 @@
+import * as Styled from "./TodoListMenu.style";
+import { Button } from "src/components/common/Button";
+import { FlexBox } from "src/components/common/FlexBox";
+import { PostModal } from "src/components/main/TodoListMenu/PostModal";
+import { Todo } from "src/api/todo/getTodo";
+import { useState } from "react";
+
+interface TodoListMenuProps {
+	todoList?: Todo[];
+	onClickTodoDetail: (nextId: string) => void;
+}
+
+export const TodoListMenu = ({
+	todoList,
+	onClickTodoDetail,
+}: TodoListMenuProps) => {
+	const [isShowModal, setIsShowModal] = useState(false);
+	const onClickAddTodo = () => {
+		setIsShowModal(true);
+	};
+
+	const hideModal = () => {
+		setIsShowModal(false);
+	};
+
+	return (
+		<>
+			<Styled.Frame>
+				<Styled.MenuHeader>목록</Styled.MenuHeader>
+				<FlexBox flexDirection='column'>
+					{todoList?.map((todo) => (
+						<Styled.MenuItem
+							key={todo.id}
+							onClick={() => onClickTodoDetail(todo.id)}
+						>
+							{todo.title}
+						</Styled.MenuItem>
+					)) ?? "리스트가 없습니다."}
+
+					<Button label='추가' width={"100%"} onClick={onClickAddTodo} />
+				</FlexBox>
+			</Styled.Frame>
+			<PostModal isShow={isShowModal} hideModal={hideModal} />
+		</>
+	);
+};
