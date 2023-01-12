81f2b47829990a2a2699631398f34ba4f507aefc
1TBhard <was1383@naver.com>
2023-01-12 15:42:13 +0900

    NAVIGATION_HEIGHT 추가 및 navigation position fixed -> 제거

--- src/components/common/MainLayout/MainLayout.style.ts
+++ src/components/common/MainLayout/MainLayout.style.ts
@@ -1,6 +1,7 @@
+import { NAVIGATION_HEIGHT } from "src/constants/style/NAVIGATION_HEIGHT";
 import styled from "styled-components";
 
 export const Frame = styled.div`
-	height: 100vh;
+	height: calc(100vh - ${NAVIGATION_HEIGHT});
 	width: 100vw;
 `;
--- src/components/main/Navigation/Navigation.style.ts
+++ src/components/main/Navigation/Navigation.style.ts
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import { Link as RouterLink } from "react-router-dom";
+import { NAVIGATION_HEIGHT } from "src/constants/style/NAVIGATION_HEIGHT";
 
 export const Frame = styled.div`
-	position: fixed;
 	display: flex;
 	justify-content: center;
 	width: 100vw;
+	height: ${NAVIGATION_HEIGHT};
 `;
 
 export const Link = styled(RouterLink)`
