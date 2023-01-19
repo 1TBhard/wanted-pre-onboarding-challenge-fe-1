325a1476c70a2328d7057ff87e78a7c3b53a0d03
1TBhard <was1383@naver.com>
2023-01-20 00:34:04 +0900

    TodoQueryClientProvider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import AuthProvider from "src/components/main/AuthProvider";
 import Navigation from "src/components/main/Navigation";
 import Pages from "src/pages";
 import { BrowserRouter } from "react-router-dom";
+import { TodoQueryClientProvider } from "src/components/TodoQueryClientProvider";
 import "src/styles/global.css";
 
 export const App = () => {
 	return (
 		<main>
 			<AuthProvider>
-				<BrowserRouter>
-					<Navigation />
-					<Pages />
-				</BrowserRouter>
+				<TodoQueryClientProvider>
+					<BrowserRouter>
+						<Navigation />
+						<Pages />
+					</BrowserRouter>
+				</TodoQueryClientProvider>
 			</AuthProvider>
 		</main>
 	);
